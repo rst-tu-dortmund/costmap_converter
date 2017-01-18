e70be6c5b5480c17b00fc4a3a5b8a35fb02abbe5
Franz Albers <Franz.Albers@tu-dortmund.de>
2017-01-18 18:50:31 +0100

    Added function definitions for CostmapToDynamicObstacles

--- include/costmap_converter/costmap_to_dynamic_obstacles.h
+++ include/costmap_converter/costmap_to_dynamic_obstacles.h
@@ -1,8 +1,10 @@
-ifndef COSTMAP_TO_DYNAMIC_OBSTACLES_H_
+#ifndef COSTMAP_TO_DYNAMIC_OBSTACLES_H_
 #define COSTMAP_TO_DYNAMIC_OBSTACLES_H_
 
 #include <ros/ros.h>
 #include <costmap_converter/costmap_converter_interface.h>
+
+#include "cv_bridge/cv_bridge.h"
 
 
 namespace costmap_converter
@@ -15,26 +17,40 @@ namespace costmap_converter
 
 class CostmapToDynamicObstacles : public BaseCostmapToPolygons
 {
-  public:
+public:
     CostmapToDynamicObstacles();
     
     ~CostmapToDynamicObstacles();
     
     virtual void initialize(ros::NodeHandle nh);
-      
-    virtual void compute();
-      
-    virtual void setCostmap2D(costmap_2d::Costmap2D* costmap);
-      
-    virtual void updateCostmap2D();
-      
-  protected:
-    
 
-   
-  private:
-       
-   
+    virtual void compute();
+
+    virtual void setCostmap2D(costmap_2d::Costmap2D* costmap);
+
+    virtual void updateCostmap2D();
+
+    void updatePolygonContainer(PolygonContainerPtr polygons);
+
+    PolygonContainerConstPtr getPolygons();
+
+    template< typename Point>
+    static void convertPointToPolygon(const Point& point, geometry_msgs::Polygon& polygon)
+    {
+          polygon.points.resize(1);
+          polygon.points.front().x = point.x;
+          polygon.points.front().y = point.y;
+          polygon.points.front().z = 0;
+    }
+
+private:
+    boost::mutex mutex_;
+    costmap_2d::Costmap2D *costmap_;
+    PolygonContainerPtr polygons_;
+    cv::Mat _frame;
+//    OpencvTracking _my_tracking;        // TODO
+//    BackgroundSubtractionClass _bs;     // TODO
+//    BlobClass _blob;                    // TODO
 }; 
 
   
--- src/costmap_to_dynamic_obstacles.cpp
+++ src/costmap_to_dynamic_obstacles.cpp
@@ -1,1 +1,50 @@
+#include "costmap_converter/costmap_to_dynamic_obstacles.h"
 
+#include <pluginlib/class_list_macros.h>
+
+PLUGINLIB_EXPORT_CLASS(costmap_converter::CostmapToDynamicObstacles, costmap_converter::BaseCostmapToPolygons)
+
+
+namespace  costmap_converter
+{
+    CostmapToDynamicObstacles::CostmapToDynamicObstacles() : BaseCostmapToPolygons()
+    {
+        costmap_ = NULL;
+    }
+
+    CostmapToDynamicObstacles::~CostmapToDynamicObstacles()
+    {
+
+    }
+
+    void CostmapToDynamicObstacles::initialize(ros::NodeHandle nh)
+    {
+        costmap_ = NULL;
+    }
+
+    void CostmapToDynamicObstacles::compute()
+    {
+        // Blob Analyse erstmal ausgelassen.. In set_own_costmap ebenfalls auskommentiert
+        // TODO: OpencvTracking _my_tracking;
+        //       BackgroundSubtractionClass _bs;
+        //       BlobClass _blob;
+    }
+
+    void CostmapToDynamicObstacles::setCostmap2D(costmap_2d::Costmap2D *costmap)
+    {
+        costmap_ = costmap;
+        updateCostmap2D();
+    }
+
+    void CostmapToDynamicObstacles::updatePolygonContainer(PolygonContainerPtr polygons)
+    {
+        boost::mutex::scoped_lock lock(mutex_);
+        polygons_ = polygons;
+    }
+
+    PolygonContainerConstPtr CostmapToDynamicObstacles::getPolygons()
+    {
+        boost::mutex::scoped_lock lock(mutex_);
+        return polygons_;
+    }
+}
