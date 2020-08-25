1eca7b957594713fb180cba5e24778cc0e3c67c1
rdeniza <61455514+rdeniza@users.noreply.github.com>
2020-08-25 18:36:44 +0200

    Update costmap_to_polygons.cpp

--- costmap_converter/src/costmap_to_polygons.cpp
+++ costmap_converter/src/costmap_to_polygons.cpp
@@ -122,13 +122,13 @@ void CostmapToPolygonsDBSMCCH::initialize(rclcpp::Node::SharedPtr nh)
 
     costmap_ = NULL;
 
-    parameter_.max_distance_ = 0.4;
+    parameter_.max_distance_ = 0.2;
     nh->get_parameter_or<double>("cluster_max_distance", parameter_.max_distance_, parameter_.max_distance_);
 
     parameter_.min_pts_ = 2;
     nh->get_parameter_or<int>("cluster_min_pts", parameter_.min_pts_, parameter_.min_pts_);
 
-    parameter_.max_pts_ = 30;
+    parameter_.max_pts_ = 1000;
     nh->get_parameter_or<int>("cluster_max_pts", parameter_.max_pts_, parameter_.max_pts_);
 
     parameter_.min_keypoint_separation_ = 0.1;
